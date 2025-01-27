f96348dfef7093a62cae5be08f00c4d5aabf0fa1
Ball <yongsuwan.sb@gmail.com>
2025-01-27 17:56:30 +0700

    change from npm to yarn

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,14 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import tsParser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { 
+    globals: globals.browser,
+    parser: tsParser,
+  }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
