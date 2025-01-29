f6a82c10d5a4cb84a80ee253b0f5990927ba193e
Teratas <tienyuop@gmail.com>
2025-01-29 19:57:14 +0700

    improve: basic img upload + form validation(Edit)

--- frontend/src/app/(logged-in)/user-profile/page.tsx
+++ frontend/src/app/(logged-in)/user-profile/page.tsx
@@ -21,16 +21,47 @@ import {
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Separator } from "@/components/ui/separator";
-import styles from './animation.module.css'
+import styles from "./animation.module.css";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+//missing
+//sort position form error
+//upload photo functionality
+// reponsive
 
 const formSchema = z.object({
+  username: z
+      .string()
+      .min(15, "Username must contain at least 15 characters")
+      .max(50, "Username must contain at most 50 characters")
+      .regex(new RegExp("^[^0-9]*$"), "Username must not contain number"),
   email: z.string().email().max(100, ""),
-  phone: z.string().min(9).max(9),
-  gender: z.enum(["Male", "Female", "Non-binary", "Other"]),
+  phone: z
+    .string()
+    .length(10, "Phone Number must contain exactly to 10 characters"),
+  gender: z.enum(["Male", "Female", "Non-Binary", "Other"]).optional(),
+  bank_account: z.string(),
+  payment_type: z.enum(["Credit/Debit", "QR_Code"], {
+    required_error: "Select Your Payment Type",
+  }),
+  card_name: z.string().optional(),
+  card_number: z.string().optional(),
+  password: z
+    .string()
+    .min(8, "Password must contain at least 8 characters")
+    .max(20, "Password must contain at most 20 characters")
+    .regex(new RegExp(".*[ -/:-@[-`{-~].*"), {
+      message: "Password should contain at least one special characters",
+    }),
+  confirmPassword: z.string(),
+  
+
 });
 type formType = typeof formSchema;
 export default function UserPage() {
@@ -39,96 +70,271 @@ export default function UserPage() {
     defaultValues: {
       email: "",
       phone: "",
+      bank_account: "",
+      card_name: "",
+      card_number: "",
+      username: '',
+      password: '',
+      confirmPassword: '',
     },
   });
-  const [gender, setGender] = useState("");
-  const [click, setClick] = useState(false)
+  /*
+  Image
+
+  use axios to post into database return it back and use that image to show
+  */
+  const [click, setClick] = useState(true);
   // redux to dispatch changes
+  const [paymentState, setPaymentState] = useState("");
+  const handleSubmit = () => {
+    //dispatch to update each case or change only display to none else hidden
+    console.log(form.getValues());
+  };
+  const [img, setImageState] = useState({
+    image:''
+  })
+
+  const onImageChange = (e:any) => {
+    if(e.target.files && e.target.files[0]){
+      // setImgState(e.target.files[0])
+      setImageState({
+        image: URL.createObjectURL(e.target.files[0])
+      })
+      // console.log(e.target.files)
+    }
+  }
   return (
-    <main className="min-h-screen flex bg-blue-400 relative items-center justify-center">
-      
-      <div className="flex justify-around w-[70%] h-[400px]">
-        <Card className='w-[400px]'>
+    <main className="font-serif min-h-screen flex bg-blue-400 relative items-center justify-center">
+      <div className="flex justify-around w-[70%] h-[60vh]">
+        <Card className="w-[400px]">
           <CardHeader>
             <CardTitle>Profile</CardTitle>
           </CardHeader>
-          <CardContent className='flex flex-col items-center'>
+          <CardContent className="flex flex-col items-center">
             <div className="text-xl font-serif font-bold flex justify-center h-[50px] items-center">
               Name
             </div>
-            <div className="bg-black w-[150px] h-[150px] rounded-full"></div>
-            <Button className='mt-5'>Upload New Photo</Button>
-            
+            {/* <div className="bg-black w-[150px] h-[150px] rounded-full">
+              <Image src={} alt=''/>
+            </div> */}
+            <Avatar className='size-40'>
+              <AvatarImage src={img.image} alt=''/>
+              <AvatarFallback className="border border-black rounded-full"></AvatarFallback>
+            </Avatar>
+            <div className="w-[100%] flex justify-center">
+              <Input
+                id="picture"
+                type="file"
+                className="mt-5 bg-black w-[50%]  after:content-['Upload'] cursor-pointer after:text-white after:text-xl after:absolute relative after:left-[50%] after:translate-x-[-50%] after:translate-y-[-50%] after:top-[50%]"
+                placeholder="Upload New Photo"
+                onChange={onImageChange}
+              />
+            </div>
           </CardContent>
         </Card>
-        <Card className="w-[500px]">
-          <CardHeader className=''>
+        <Card className="w-[500px] relative">
+          <CardHeader className="">
             <CardTitle>Edit Profile</CardTitle>
           </CardHeader>
-          <CardContent className=" flex flex-col relative">
-          <div className="flex flex-row w-[50%] relative justify-between">
-              <Link href={""} onClick={() => setClick(true)} className={`${styles.divLine} hover:after:scale-x-100 after:bg-blue-200 after:content-[''] after:w-[30%] after:h-[3px] after:absolute after:bottom-[-60%] after:left-0 ${(click) ? 'after:scale-x-100' : 'after:scale-x-0'}`}>User Info</Link>
-              <Link href={""} onClick={() => setClick(false)} className={`${styles.divLines} hover:after:scale-x-100 cursor-pointer after:bg-blue-200 after:content-[''] after:w-[58%] after:h-[3px] after:absolute after:bottom-[-60%] after:left-[43%] ${(!click) ? 'after:scale-x-100' : 'after:scale-x-0'}`}>Billing Information</Link>
+          <CardContent className=" flex flex-col relative h-full">
+            <div className="flex flex-row w-[60%] relative justify-between">
+              <Link
+                href={""}
+                onClick={() => setClick(true)}
+                className={`${styles.divLine} hover:after:scale-x-100 after:bg-blue-200 after:content-[''] after:w-[27%] after:h-[3px] after:absolute after:bottom-[-60%] after:left-0 ${click ? "after:scale-x-100" : "after:scale-x-0"}`}
+              >
+                User Info
+              </Link>
+              <Link
+                href={""}
+                onClick={() => setClick(false)}
+                className={`${styles.divLines} hover:after:scale-x-100 cursor-pointer after:bg-blue-200 after:content-[''] after:w-[58%] after:h-[3px] after:absolute after:bottom-[-60%] after:left-[47%] ${!click ? "after:scale-x-100" : "after:scale-x-0"}`}
+              >
+                Billing Information
+              </Link>
             </div>
             <Separator className="my-3" />
-            {
-              (click) ? (<Form {...form}>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(handleSubmit)}
+                className="h-[80%]"
+              >
                 <fieldset
                   disabled={form.formState.isSubmitting}
-                  className="flex flex-row w-[100%] flex-wrap justify-between"
+                  className="h-[100%] flex items-start"
                 >
-                  <FormItem className="">
-                    <FormLabel>Username</FormLabel>
-                    <Input type="text" />
-                  </FormItem>
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email</FormLabel>
+                  <div
+                    className={`${click ? "" : "hidden"} flex flex-row w-[100%] flex-wrap justify-between `}
+                  >
+                    <FormField name='username' control={form.control} render={({field}) => (
+                      <FormItem className="w-[40%]">
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="text" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                    )}/>
+                    
+                    <FormField
+                      control={form.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem className="w-[40%]">
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input {...field} type="text" />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="phone"
+                      render={({ field }) => (
+                        <FormItem className="w-[40%]">
+                          <FormLabel>Phone Number</FormLabel>
+                          <FormControl>
+                            <Input {...field} type="text" />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      name="gender"
+                      control={form.control}
+                      render={({ field }) => (
+                        <FormItem className=" w-[40%]">
+                          <FormLabel className="r">Gender</FormLabel>
+                          <FormControl>
+                            <Select
+                              onValueChange={field.onChange}
+                              value={field.value}
+                            >
+                              <SelectTrigger className="">
+                                <SelectValue placeholder="Select Your Gender" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectGroup>
+                                  <SelectItem value="Male">Male</SelectItem>
+                                  <SelectItem value="Female">Female</SelectItem>
+                                  <SelectItem value="Non-Binary">
+                                    Non-Binary
+                                  </SelectItem>
+                                  <SelectItem value="Other">Other</SelectItem>
+                                </SelectGroup>
+                              </SelectContent>
+                            </Select>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField name="password" control={form.control} render={({field}) => (
+                      <FormItem className=" w-[40%]">
+                        <FormLabel>Password</FormLabel>
                         <FormControl>
-                          <Input {...field} type="text" />
+                          <Input type='text' {...field}/>
                         </FormControl>
                         <FormMessage />
                       </FormItem>
-                    )}
-                  />
-                  <FormField
-                    control={form.control}
-                    name="phone"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Phone Number</FormLabel>
+                    )}/>
+                    <FormField name="confirmPassword" control={form.control} render={({field}) => (
+                      <FormItem className=" w-[40%]">
+                        <FormLabel>Confirm Password</FormLabel>
                         <FormControl>
-                          <Input {...field} type="text" />
+                          <Input type='text' {...field}/>
                         </FormControl>
                         <FormMessage />
                       </FormItem>
+                    )}/>
+                    <FormItem className="flex flex-col w-[100%] h-[100px] justify-center">
+                      <Button type="submit">Update Info</Button>
+                    </FormItem>
+                  </div>
+                  <div className={`${click ? "hidden" : ""} w-[100%] `}>
+                    <FormField
+                      name="bank_account"
+                      control={form.control}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Bank Account</FormLabel>
+                          <FormControl>
+                            <Input type="text" {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      name="payment_type"
+                      control={form.control}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Default Payment Type</FormLabel>
+                          <FormControl>
+                            <Select
+                              value={field.value}
+                              onValueChange={(e) => {
+                                field.onChange(e);
+                                setPaymentState(e);
+                              }}
+
+                              // {...form.register("payment_type")}
+                            >
+                              <SelectTrigger>
+                                <SelectValue placeholder="Select" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectGroup>
+                                  <SelectItem value="QR_Code">
+                                    QR Code
+                                  </SelectItem>
+                                  <SelectItem value="Credit/Debit">
+                                    Credit/Debit
+                                  </SelectItem>
+                                </SelectGroup>
+                              </SelectContent>
+                            </Select>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    {/* <div>{paymentState}</div> */}
+                    {paymentState === "Credit/Debit" ? (
+                      <div className="  flex flex-col justify-around py-4 h-[200px]">
+                        <FormField
+                          name="card_name"
+                          control={form.control}
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Name on card</FormLabel>
+                              <Input type="text" />
+                            </FormItem>
+                          )}
+                        />
+                        <FormField
+                          name="card_number"
+                          control={form.control}
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Card Number</FormLabel>
+                              <Input type="text" />
+                            </FormItem>
+                          )}
+                        />
+                      </div>
+                    ) : (
+                      <div></div>
                     )}
-                  />
-                  <FormItem className="flex flex-col justify-end">
-                    <FormLabel className=' flex items-center'>Gender</FormLabel>
-                    <Select onValueChange={setGender}> 
-                      <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Select Your Gender" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectItem value="Male">Male</SelectItem>
-                          <SelectItem value="Female">Female</SelectItem>
-                          <SelectItem value="Non-Binary">Non-Binary</SelectItem>
-                          <SelectItem value="Other">Other</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  </FormItem>
-                  <FormItem className="flex flex-col w-[100%] h-[100px] justify-center">
-                    <Button>Update Info</Button>
-                  </FormItem>
+                  </div>
                 </fieldset>
-              </Form>) : (<div></div>)
-            }
+              </form>
+            </Form>
           </CardContent>
         </Card>
       </div>
