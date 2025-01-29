a854ecef6135c519b3839f2d4954374ec0563b30
boomenghub <12evolte@gmail.com>
2025-01-29 18:50:14 +0700

    debug: debug api for register and login but cannot create more user due to schema conflict

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import AuthService from '../services/authService';
+import authService from '../services/authService';
 import { RegisterDTO, LoginDTO } from '../dtos/authDTO';
 // import jwt from 'jsonwebtoken'
 
@@ -8,12 +8,22 @@ import { RegisterDTO, LoginDTO } from '../dtos/authDTO';
 // @access Public
 export const createUser = async (req: Request, res: Response ) => {
     try {
-        const {username, password, role}: RegisterDTO = req.body;
+        const { username, password, role }: RegisterDTO = req.body;
 
-        const user = await AuthService.registerUser(username, password, role);
+        if (!username || !password || !role) {
+            res.status(400).json({ success: false, msg: "Please provide all required fields." });
+            return;
+        }
+
+        const user = await authService.registerUser(username, password, role);
+
         res.status(201).json({ success: true, data: user });
-    } catch (error) {
-        res.status(400).json({ success: false, msg: error.msg });
+    } catch (err) {
+        console.error("Error in registerUser:", err);
+        
+        const errorMessage = (err as Error).message || "Internal server error.";
+
+        res.status(500).json({ success: false, msg: errorMessage });
     }
 };
 
@@ -24,10 +34,17 @@ export const loginUser = async (req: Request, res: Response) => {
     try {
         const { username, password }: LoginDTO = req.body;
 
-        const result = await AuthService.loginUser(username, password);
+        if(!username || !password){
+            res.status(400).json({ success: false, msg: "Please provide all required fields." });
+            return;
+        }
+
+        const result = await authService.loginUser(username, password);
         res.status(200).json({ success: true, msg: "Login successful.", data: result });
         // res.status(200).json({ success: true, msg: "Login successful.", data: {user, token} });
-    } catch (error) {
-        res.status(400).json({ success: false, msg: error.msg });
+    } catch (err) {
+        const errorMessage = (err as Error).message || "Internal server error.";
+        
+        res.status(400).json({ success: false, msg: errorMessage});
     }
 };
