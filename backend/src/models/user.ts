2882f0d083d37232a90acad7912bfe45d70bd5b4
PunnawitAck3003 <punnawitack@gmail.com>
2025-01-29 19:52:23 +0700

    imp: models/user email - sparse: true

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -32,7 +32,8 @@ const UserSchema: Schema = new Schema({
   email: {
     type: String,
     //required: [true, 'Please add an email'],
-    //unique: true,
+    unique: true,
+    sparse: true,
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       'Please add a valid email',
