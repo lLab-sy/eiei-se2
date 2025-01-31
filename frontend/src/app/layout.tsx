ef000ac21ef7c826938111927a9cabf3bc5892bc
miuleto <miuleto@outlook.com>
2025-01-31 16:00:54 +0700

    improve: change layout to include header and footer in every page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <NavBar />
         {children}
+        <Footer />
         <Toaster />
       </body>
     </html>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,7 @@
-import NavBar from "@/components/NavBar";
 import Banner from "@/components/Banner";
 import ShortKeyList from "@/components/ShortKeyList";
 import MovieList from "@/components/MovieList";
 import ProfessionalList from "@/components/ProfessionalList";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -12,7 +10,6 @@ export default function Home() {
       <ShortKeyList />
       <MovieList />
       <ProfessionalList />
-      <Footer />
     </div>
   );
 }
