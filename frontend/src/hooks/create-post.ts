077c60d3a0a4a9fd58fe555d99ec820d30c24311
miuleto <miuleto@outlook.com>
2025-02-06 00:24:45 +0700

    feat: (draft) API integrate

---
+++ frontend/src/hooks/create-post.ts
@@ -0,0 +1,29 @@
+import AxiosInstance from "@/lib/axiosInstance"
+type postData = {
+    postProjectRoles: string[],
+    postName: string,
+    postMediaType: string,
+    postDescription: string,
+    postImages: string[],
+}
+
+type param = {
+    postData: postData,
+    token: string,
+}
+
+
+
+export default async function createPost({ postData, token }: param) {
+    const response = await AxiosInstance.post('/api/v1/posts', {
+        postData
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        }
+      }
+    )
+    return response
+  }
+  
