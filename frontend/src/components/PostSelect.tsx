af590cb1e5468d30b63b937b03f038ae0cb80291
pok1220 <pokkung1220@gmail.com>
2025-02-14 21:36:42 +0700

    add sth

---
+++ frontend/src/components/PostSelect.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+
+export default function PostSelect(){
+    return(
+        <div>
+            <Image
+                src={}
+                width={0}
+                height={0}
+                className=""
+            />
+
+
+        </div>
+    )
+}
