b5297329d76639ffa1c5a0fd1222b547b604a175
Ball <yongsuwan.sb@gmail.com>
2025-02-23 17:19:55 +0700

    feat: rate limit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,6 +15,7 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes'
 import cloudRouter from './routes/cloudRoutes'
 import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
 
 dotenv.config();
 
@@ -33,6 +34,18 @@ app.use(helmet());
 app.use(cors({origin: true, credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `windowMs`
+  message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true, // Send rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware globally (for all routes)
+app.use(limiter);
+
 
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-store');  // Prevent caching
