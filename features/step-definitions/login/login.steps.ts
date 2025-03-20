4b2fa75eefe3327756f5d943691c5c2bfddff88d
miuleto <miuleto@outlook.com>
2025-03-20 21:13:01 +0700

    Change: Create Offer in Playwright (except show in producer)

--- features/step-definitions/login/login.steps.ts
+++ features/step-definitions/login/login.steps.ts
@@ -24,7 +24,7 @@ Given("the {string} is logged in", async function (role) {
     await page.goto("/",{waitUntil: 'load', timeout: 0});
     await page.waitForLoadState("domcontentloaded");
 
-    await page.getByRole("button").click();
+    await page.getByRole('banner').getByRole('button').click();
     await page.getByText("Login").click();
     await page.getByRole('textbox', { name: 'username' }).fill(username);
     await page.getByRole('textbox', { name: 'password' }).fill(password);
