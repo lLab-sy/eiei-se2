2a6f9e761b46c6476a9934d7abf1fce39ee3e078
pok1220 <pokkung1220@gmail.com>
2025-03-25 19:45:07 +0700

    fix submission by professional

--- features/step-definitions/my-post/markSubmission.ts
+++ features/step-definitions/my-post/markSubmission.ts
@@ -17,23 +17,37 @@ When('the professional view his\\/her {string}', async function (string) {
         await page.getByRole('option', { name: 'in-progress' }).click();
         
         const container=await page.getByTestId('Post-cards-container')
-        const countWorking=await container.locator('a[href]').count();
-
+        // const targetCard =await page.getByText(string)
+        // const countWorking=await container.locator('a[href]').count();
+        await expect(page.getByText(string)).toBeVisible();
+        await page.getByText(string).click()
+        // console.log("num",page.getByText(string).count())
     }
 
   });
 
-Then('Professional should click at {string}', async function (string) {
+Then('Professional should click at {string} and see recent task', async function (string) {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const page = customWorld.page;
+    if (page){
+      await page.waitForLoadState('domcontentloaded')
+      await page.waitForTimeout(500);
+      const beforeAdd=await page.getByText("/3").count()
+      await page.getByText(string).click(string)
+      await page.getByText("Confirm").click()
+      const afterAdd=await await page.getByText("/3").count()
+      expect(afterAdd-beforeAdd).toEqual(1)
+    }
   });
 
-Then('Professional can see recent task {string} in post detail', async function (string) {
+  Then('Professional can see all his task {string} times in post detail and has no {string}', async function (string, string2) {
     // Write code here that turns the phrase above into concrete actions
-    return 'pending';
-  });
-
-Then('Professional can see all his task {string} times in post detail', async function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+    const page = customWorld.page;
+    if (page){
+      await page.waitForLoadState('domcontentloaded')
+      await page.waitForTimeout(1000);
+      const count=await page.getByText("/3").count()
+      expect(count).toEqual(parseInt(string))
+      await expect(page.getByText(string2)).not.toBeVisible();
+    }
   });
