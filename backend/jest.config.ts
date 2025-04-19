16b0ea93eedfaaaa9798880dad12726b29f96abf
pok1220 <pokkung1220@gmail.com>
2025-04-19 19:26:42 +0700

    add service and repo test (only authen)

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: ['src/controllers/**/authController.ts','src/services/**/authService.ts'], // เก็บ coverage เฉพาะ controller ก็ได้
+  collectCoverageFrom: ['src/controllers/**/authController.ts','src/services/**/authService.ts', "src/repositories/userRepository.ts"], // เก็บ coverage เฉพาะ controller ก็ได้
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
   testPathIgnorePatterns: ['/node_modules/'],
